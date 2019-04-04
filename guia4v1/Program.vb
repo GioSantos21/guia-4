fa294417ba6fcced5368bac2bc9fb5c29132b43f
geovani_santos <geovanny12016@gmail.com>
2019-04-04 13:16:16 -0600

    guia4SegundoCommit

--- guia4v1/Program.vb
+++ guia4v1/Program.vb
@@ -12,6 +12,19 @@ Module Program
                     Console.WriteLine(i)
                     i = i + 2
                 End While
+                Console.WriteLine()
+            ElseIf respuesta = 2 Then
+                Console.WriteLine("Ingrese un número entero positivo: ")
+                Dim numero As Integer = Console.ReadLine()
+                Dim inicio As Integer = 1
+                Dim total As Integer = 0
+                Console.WriteLine()
+                While numero >= inicio
+                    total = inicio + numero
+                    Console.WriteLine(total)
+                    inicio = inicio + 1
+                End While
+                Console.WriteLine()
             End If
         End While
     End Sub
